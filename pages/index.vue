bb02d9200182804d25327e95ca6f535120dd231a
Edwin Bosveld <edwin@inwy.nl>
2021-04-10 00:54:21 +0200

    Add some styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="container">
-    <div>
+  <div class="container flex-col">
+    <div class="w-screen place-content-center flex-grow flex flex-col">
       <h1 class="text-4xl text-gray-500">Hoeveel is er al gevaccineerd?</h1>
       <vaccination-counter />
     </div>
+    <footer class="p-2 w-screen flex flex-row">
+      <div class="text-gray-400">
+        Data van <a class="underline" href="https://ourworldindata.org/covid-vaccinations">Our World in Data</a>.
+      </div>
+    </footer>
   </div>
 </template>
 

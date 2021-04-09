e5c9f61eea2e30479eb40b109befd381b66363ce
Edwin Bosveld <edwin@inwy.nl>
2021-04-09 23:08:04 +0200

    Fix directory structure

---
+++ netlify-plugins/fetch-remote-content/index.js
@@ -0,0 +1,16 @@
+
+const axios  = require('axios');
+const parse  = require('csv-parse/lib/sync')
+const fs      = require('fs');
+
+module.exports = {
+    onPreBuild: async () => {
+        let fileUrl = 'https://raw.githubusercontent.com/owid/covid-19-data/master/scripts/scripts/vaccinations/automations/output/Netherlands.csv'
+
+        const parserOpts = { columns: true, skip_empty_lines: true};
+        const data = await axios.get(fileUrl).then(response => { return response.data; })
+        const parsedRecords = await parse(data, parserOpts);
+        const [lastItem] = parsedRecords.slice(-1);
+        await fs.writeFileSync('data/vaccinations.json', JSON.stringify(lastItem));
+    },
+}

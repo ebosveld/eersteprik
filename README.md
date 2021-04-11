3cac7e7b0bbd818c2ecb6fc1f3fb902ce656ca88
Edwin Bosveld <edwin@inwy.nl>
2021-04-11 16:25:47 +0200

    Add github link and update readme

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Eerste prik
+![Netlify](https://img.shields.io/netlify/9c69f98b-ec13-4cf3-ac6b-973c7eaa9f38) ![GitHub](https://img.shields.io/github/license/ebosveld/eersteprik) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com) ![Node](https://img.shields.io/badge/node-%3E%3D10.12.0-brightgreen)
 
-## Purpose
-To combat the Coronavirus crisis, Dutch minister for Health Hugo de Jonge declared that every adult who was willing, could get the first shot of the vaccine by July 1st. This dashboard is tracking if we're on target to reach that goal.
+
+To combat the Coronavirus crisis, Dutch Minister of Health, Welfare and Sport, Hugo de Jonge promised to vaccinate every adult with at least one shot by July 1st. This website is tracking if we're on target to reach that goal.
+
+## Hosting
+Hosting is done by Netlify. With every pull request, a preview is made to see the end result. When a commit is made to the main branch, the site will be automatically deployed.
 
 ## Build Setup
+The project uses Nuxt.js and Yarn. So make sure you've installed Yarn and Node, and then just install the dependencies and start the development server with the following commands.
 
 ```bash
 # install dependencies
@@ -11,13 +16,16 @@ $ yarn install
 
 # serve with hot reload at localhost:3000
 $ yarn dev
-
-# build for production and launch server
-$ yarn build
-$ yarn start
-
-# generate static project
-$ yarn generate
 ```
 
-For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+## Data
+Currently our data is being crawled from [Our World in Data](https://ourworldindata.org/covid-vaccinations), which in turn gets it from the RIVM. So in the future it might be wise crawl it directly from the RIVM site, as it's being updated every Tuesday at 4PM CET.
+
+The data is stored in `/static/data` and a version of the data is stored in this repository for ease of starting development. With every build by Netlify the data is being pull from the sources using a build plugin.
+
+## Contributing
+
+## Credits
+
+## License
+[MIT](LICENSE) Copyright (c) 2021 Edwin Bosveld
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="container flex-col">
+  <div class="container flex flex-col mx-auto h-screen">
     <div class="bg-pink-700 text-white w-40 h-14 ml-6 flex justify-center">
       <span class="pt-4 font-bold uppercase tracking-wider">EerstePrik<span class="font-normal">.nl</span></span>
       </div>
-    <div class="w-screen place-content-center flex-grow flex flex-col">
+    <div class="w-screen place-content-center flex-grow flex flex-col text-center">
       <!-- <h1 class="text-4xl text-gray-500">Hoeveel mensen hebben de eerste prik gehad?</h1> -->
       <vaccination-counter />
     </div>
-    <footer class="p-2 w-screen flex flex-row text-xs">
+    <footer class="pb-2 px-3 w-screen flex flex-row text-xs justify-between items-center">
       <div class="text-gray-400">
         Bron: <a class="underline" href="https://www.rivm.nl/covid-19-vaccinatie/cijfers-vaccinatieprogramma">RIVM</a> (via <a class="underline" href="https://ourworldindata.org/covid-vaccinations">Our World in Data</a>) &amp; CBS (bevolkingstelling op 1 maart 2021)
+      </div>
+      <div class="self-end">
+        <a href="https://github.com/ebosveld/eersteprik" class="text-gray-400 hover:text-gray-500 transition-colors duration-200">
+          <svg width="24" height="24" viewBox="0 0 16 16" fill="currentColor"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
+        </a>
       </div>
     </footer>
   </div>
@@ -24,11 +29,11 @@ export default Vue.extend({})
 
 <style>
 
-.container {
+/* .container {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   text-align: center;
-}
+} */
 </style>
